a2410f5dcb42c57d8d23c78b2887ddf9d13df4c4
sagautam5 <sagautam5@gmail.com>
2025-12-15 23:14:58 +0545

    setup(tests): using pest and orchestra

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,5 @@
+<?php
+
+it('returns a successful response', function () {
+    expect(true)->toBeTrue();
+});
---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sagautam5\EmailBlocker\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
